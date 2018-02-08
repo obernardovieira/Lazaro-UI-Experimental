1f2535c8e0823e52893eb954b181cafd744c3829
Bernardo Vieira <bernardo5@live.com.pt>
2018-02-08 00:11:13 +0000

    move to ejs and reformulate

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-const electron = require('electron')
-const app = electron.app
+const express = require('express')
+const app = express()
+const window = require('./modules/window.js')
+const electron = require('electron').app
 
-const window = require('./window.js')
+app.set('view engine', 'ejs')
+app.use("/styles", express.static(__dirname + "/styles"))
+app.get('/', window.render)
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
 
-app.on('ready', window.createWindow)
+electron.on('ready', window.create)
---
+++ modules/window.js
@@ -0,0 +1,27 @@
+const electron = require('electron')
+const cmd = require('node-cmd')
+const TreeView = require('./treeview.js');
+const DataSaver = require('./datasaver')
+var {app, BrowserWindow} = electron
+
+module.exports = {
+
+    create: function() {
+        let mainWindow = new BrowserWindow({width: 800, height: 600})
+
+        mainWindow.loadURL('http://localhost:3000')
+
+        //cmd.run('gnome-terminal -x sh -c \'command1; command2; exec bash\'')
+        
+        
+        mainWindow.on('closed', function () {
+            mainWindow = null
+        })
+    },
+
+    render: function(request, response) {
+        var treeView = new TreeView()
+        var treeSet = treeView.build(DataSaver.load())
+        response.render('index', { tableTree: treeSet })
+    }
+}
